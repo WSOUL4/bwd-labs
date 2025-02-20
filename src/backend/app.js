864455f9cf474ae2c432016fde85cc623b684295
WSOUL4 <mitrinov4124@gmail.com>
2025-02-20 17:13:56 +0400

    build-refactor
    Изменена структура каталога проекта.
    Добавлен запуск сервера, и первый маршрут.
    Положено начало для создание соединения с БД.

---
+++ src/backend/app.js
@@ -0,0 +1,36 @@
+// подключение express
+//const express = require("express");
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+function apiDefault(app){
+
+    app.get("/", function(request, response){
+     
+        // отправляем ответ
+        response.json({msg: 'Simple msg2'})
+        //response.send("<h2>Привет Express!</h2>");
+    });
+}
+function appServerLaunch(){
+// создаем объект приложения
+const app = express();
+app.use(cors())//middleware
+app.use(express.json())//middleware
+dotenv.config()//Для чтения из .env
+// определяем обработчик для маршрута "/"
+apiDefault(app);
+
+// начинаем прослушивать подключения на  порту
+
+const port = process.env.PORT;
+//const port =8080;
+
+app.listen(port, (error) => {
+    if (error) throw error;
+    console.info(`Ready on port ${port}`);
+  });
+
+}
+
+export { appServerLaunch, apiDefault };//можно апи функцию не экспортировать она типо если из экспортируемой берётся, то взаимосвязанные хватает
