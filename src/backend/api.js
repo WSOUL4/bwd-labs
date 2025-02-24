d8fa15e6dae55b027eae60f8eac51651c5f9fa64
WSOUL4 <mitrinov4124@gmail.com>
2025-02-24 12:58:58 +0400

    feature
    Созданы ненастроенные маршруты.
    Добавлена функция для получения параметров запроса без тела.

--- src/backend/api.js
+++ src/backend/api.js
@@ -1,8 +1,58 @@
 import {EstablishConnection,conn} from  './config/db.js'//'./src/backend/config/db.js'
 
+function getParameterByName(name, url) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+
+
 function apiConfiguration(app){
 
-    app.get("/users", apiGETusers)
+
+    app.get("/users?",apiGETuserById);
+    app.get("/events?",apiGETeventById);
+    app.get("/users", apiGETusers);
+    app.get("/events", apiGETevents);
+    app.post("/users",apiPOSTuser);
+    app.post("/events",apiPOSTevent);
+    app.put("/events/",apiPUTevent);
+    app.delete("/events/",apiDELETEevent);
+}
+
+
+
+function apiGETuserById(request, response) {
+    console.log(getParameterByName('id',request.url));
+    response.json('got it');
+    /*
+    request.json().then((data) => {
+        console.log(data);
+    });
+
+     */
+
+
+}
+function apiGETeventById(request, response) {
+
+}
+
+function apiPOSTuser(request, response) {
+
+}
+function apiPOSTevent(request, response) {
+
+}
+
+function apiPUTevent(request, response) {
+
+}
+function apiDELETEevent(request, response) {
 
 }
 
@@ -13,15 +63,34 @@ function apiGETusers(request, response) {
 
         conn.query('SELECT name, email, "createdAt", id\n' +
             '\tFROM public."user";', function(err, results, fields) {
-            console.log(err);
+
             //console.log(results);  // собственно данные
-            response.json(results.rows);
+            if (err){
+                console.log(err);
+                response.json(err,"Ошибка запроса");
+            }else{
+            response.json(results.rows);}
            // console.log(fields);  // мета-данные полей
         });
 
         conn.end;
 
 
+}
+function apiGETevents(request, response) {
+    conn.query('SELECT title, description, date, "createdBy", id\n'+
+   'FROM public.event;', function(err, results, fields) {
+
+        //console.log(results);  // собственно данные
+        if (err){
+            console.log(err);
+            response.json(err,"Ошибка запроса");
+        }else{
+            response.json(results.rows);}
+        // console.log(fields);  // мета-данные полей
+    });
+
+    conn.end;
 }
 
 export {apiConfiguration}
