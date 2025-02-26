0cd3ebba3fed5674540be83981125cf6813a1a35
WSOUL4 <mitrinov4124@gmail.com>
2025-02-26 14:36:42 +0400

    feature
    Добавлено логирование в консоль сервера используя пакет morgan

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 //import swaggerUi from 'swagger-ui-express'
+import logger from 'morgan'
 import {swaggerSpec,swaggerUi} from './swagger.js'
 import {appServerLaunch} from './src/backend/app.js'
 import {EstablishConnection,conn} from './src/backend/config/db.js'
@@ -11,6 +12,7 @@ import {apiConfiguration} from './src/backend/api.js'
 
 
 let app=appServerLaunch();
+app.use(logger('dev'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 EstablishConnection();//ЭТО НУЖНОЕ
 
